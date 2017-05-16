52e221fb7583d836a09761e4bcbf97c7ee207b0f
barthclem <barthclem@gmail.com>
2017-05-16 03:50:58 +0100

    created participant_event join table migration

---
+++ migrations/20170515184233_create_participant_event_join_table.js
@@ -0,0 +1,12 @@
+'use strict';
+exports.up = function(knex) {
+  return knex.schema.createTableIfNotExists('participant_event',( table) => {
+      table.increments('id');
+      table.integer('participant_id').unsigned().notNullable().references('participant.id');
+      table.integer('event_id').unsigned().notNullable().references('event.id');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.dropTableIfExists('participant_event');
+};
